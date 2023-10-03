eeb3fab0c74b731250f4f5148b43cdc81a8d2a9d
psnem <piyush.sharma.1992@gmail.com>
2023-10-04 00:30:59 +0530

    Collection started

--- src/Arraylist/ALwithClass.java
+++ src/Arraylist/ALwithClass.java
@@ -4,36 +4,31 @@ import java.util.ArrayList;
 import java.util.Iterator;
 
 public class ALwithClass {
-	int id;
-	String name;
-	int age;
-	public ALwithClass(int id,String name,int age) {
-		this.id=id;
-		this.name=name;
-		this.age=age;
-	}
+	
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		ALwithClass st1=new ALwithClass(1,"Piyush", 30);
-		ALwithClass st2=new ALwithClass(2,"Chandu", 26);
-		ALwithClass st3=new ALwithClass(3,"Durgesh", 25);
-		ALwithClass st4=new ALwithClass(4,"Neel", 23);
+		Student st1=new Student(1,"Piyush", 30);
+		Student st2=new Student(2,"Chandu", 26);
+		Student st3=new Student(3,"Durgesh", 25);
+		Student st4=new Student(4,"Neel", 23);
 		
-		ArrayList<ALwithClass> list=new ArrayList<ALwithClass>();
+		ArrayList<Student> list=new ArrayList<Student>();
 		list.add(st1);
 		list.add(st2);
 		list.add(st3);
 		list.add(st4);
-		System.out.println(list);
+		
+		//System.out.println(list);
 		
 		System.out.println("traversing by iterator");
+		
 		Iterator itr=list.iterator();
 		while(itr.hasNext())
 		{
-			ALwithClass al=(ALwithClass)itr.next();
-			System.out.println(al);
+			Student al=(Student)itr.next();
+			System.out.println(al.name+" "+al.id+" "+al.age);
 			
 		}
 		
